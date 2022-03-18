09007ff4582d225cc3c7ac044b6e5f51cfea6174
Scott Richards <scott.richards@glassdoor.com>
2022-03-18 08:56:44 -0700

    Initial Commit

---
+++ MetalMac/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MetalMac
+//
+//  Created by Scott Richards on 3/18/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
